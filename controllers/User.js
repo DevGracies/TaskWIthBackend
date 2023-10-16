8d85640af59f03f3cdfc8102a44ff741a94151a1
Grace <graceadegunle16@gmail.com>
2023-10-16 19:12:33 +0100

    commit

--- controllers/User.js
+++ controllers/User.js
@@ -2,6 +2,46 @@ import httpStatus from "http-status";
 import UserModel from "../models/User.js";
 import { generateCode } from "../utils/generateUniqueCode.js";
 import { serializeUser } from "../utils/serializeUser.js";
+import { generateToken } from "../utils/jwt-token.js";
+import bcrypt from "bcrypt";
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //confirm that email exist indb
+    const userExist = await UserModel.findOne({ email: email });
+    if (!userExist) {
+      res.status(httpStatus.NOT_FOUND).json({
+        status: "error",
+        payload: "User does not exist, please register",
+      });
+      return;
+    }
+
+    //check that the supplied password matched the existing password for the account in db
+
+    const decodeRes = await bcrypt.compare(password, userExist.password);
+    if (!decodeRes) {
+      res.status(httpStatus.FORBIDDEN).json({
+        status: "error",
+        payload: "Credential does not match",
+      });
+      return;
+    }
+
+    res.status(httpStatus.OK).json({
+      status: "success",
+      payload: serializeUser(userExist),
+      token: generateToken(userExist._id, userExist.email),
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      status: "error",
+      payload: error.message,
+    });
+  }
+};
+
 export const createUser = (req, res) => {
   res.status(httpStatus.OK).json({
     status: "success",
